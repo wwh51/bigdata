eae4cee44b58603245f00392e67ae8a4e9062df1
David Wang <weihuaw@gmail.com>
2014-04-02 21:25:00 +1100

    Update Create_Big_File.py

--- Create_Big_File.py
+++ Create_Big_File.py
@@ -1,23 +1,23 @@
 import os
 import sys
 
-def generateRandom(rangeFrom, rangeTo):  
-    import random  
-    return random.randint(rangeFrom,rangeTo)  
-  
-def generageMassiveIPAddr(fileLocation,numberOfLines):  
-    IP = []  
-    file_handler = open(fileLocation, 'a+')  
-    for i in range(numberOfLines):  
-        IP.append('10.197.' + str(generateRandom(0,255))+'.'+ str(generateRandom(0,255)) + '\n')  
-  
-    file_handler.writelines(IP)  
-    file_handler.close()  
-  
-if __name__ == '__main__':  
-    from time import ctime  
+def generateRandom(rangeFrom, rangeTo):
+    import random
+    return random.randint(rangeFrom,rangeTo)
+
+def generageMassiveIPAddr(fileLocation,numberOfLines):
+    IP = []
+    file_handler = open(fileLocation, 'a+')
+    for i in range(numberOfLines):
+        IP.append('10.197.' + str(generateRandom(0,255))+'.'+ str(generateRandom(0,255)) + '\n')
+
+    file_handler.writelines(IP)
+    file_handler.close()
+
+if __name__ == '__main__':
+    from time import ctime
     print(ctime() )
-    for i in range(10):  
+    for i in range(10):
         print('  ' + str(i) + ": " + ctime() )
-        generageMassiveIPAddr('d:\\massiveIP.txt', 100000)  
+        generageMassiveIPAddr('c:\\temp\\massiveIP.txt', 1000000)
     print(ctime())
